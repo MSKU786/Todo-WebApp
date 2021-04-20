0ee9d134665205a4aa4ca87978e92af99e8e90b1
MANISH SINGH <manishsingh332000@gmail.com>
2021-04-20 06:59:20 +0530

    Working on layout of page

---
+++ assets/js/script1.js
@@ -0,0 +1,5 @@
+$(function() {
+    $( "#my_date_picker" ).datepicker({
+        defaultDate:"09/22/2020"
+    });
+});
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
-const port = 8000
+const port = 4000
+
 
 
 //Use express router
 app.use('/',require('./routes'));
+
+//Setting up the views
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+app.use(express.static('assets'));
+
+app.post('/add_task', function(req, res){
+    // contactList.push(
+    //     {
+    //         name: req.body.name,
+    //         phone: req.body.phone
+    //     }
+    // )
+    // Contact.create({
+    //     name: req.body.name,
+    //     phone: req.body.phone
+    // }, function(err, newContact)
+    // {
+    //     if(err){
+    //         console.log('error in creating contact');
+    //         return;
+    //     }
+    //     console.log("******^&&",newContact);
+    //     return res.redirect('back');  
+    // })
+    console.log(req.body);
+    return res.redirect('back');
+});
 
 app.listen(port, function(err){
     if(err)
