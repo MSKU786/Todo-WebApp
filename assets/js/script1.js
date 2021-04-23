5d5ca19ec8f6748ad16c975c1c0cf2d378c5d8be
MANISH SINGH <manishsingh332000@gmail.com>
2021-04-23 12:53:45 +0530

    Working on styling

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -4,5 +4,50 @@ $(function() {
     });
 });
 
+// $(function(){
+//     var S = $('.category').text();
+//     var ar = new Array();
+//     var start =0;
+//     var end =0;
+//     for(let i=1;i<S.length;i++)
+//     {
+//         if(S.charCodeAt(i)>=65 && S.charCodeAt(i)<=90)
+//         {
+//             var sub = S.substring(start,i);
+//             start = i;
+//             ar.push(sub);
+//         }
+//     }
+//     let subs = S.substring(start)
+//     ar.push(subs)
+//     console.log(ar);
+//     for(let i in ar)
+//     {
+//         switch(ar[i])
+//         {
+//         case "Personal": $('.category>span').css({
+//                         'background-color' : 'yellow'
+//                     });
+//                     break;
 
-$('')
+//         case "Work" :   $('.category>span').css({
+//                     'background-color' : 'red'
+//                     });
+//                     break;
+
+//         case "Other" : $('.category>span').css({
+//                     'background-color' : 'green'
+//                     });
+//                     break;
+
+//         case "School" : $('.category>span').css({
+//                     'background-color' : 'blue'
+//                     });
+//                     break;
+
+//         case "Cleaning": break;
+
+//         default: $('c')
+//         }
+//     }
+// });
