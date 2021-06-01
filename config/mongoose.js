5f84282dbe7e1ff85e95b03e0a2a18f1b214b11a
Manish Singh <manishsingh332000@gmail.com>
2021-06-01 21:05:20 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 //Connecting moongooose to the database name todo_db
 
-mongoose.connect(MONGO_URI,
+mongoose.connect(process.env.MONGO_URI,
     { useNewUrlParser: true,
       useUnifiedTopology: true }
     );
